ef856a8cce9591a3d698573bc3afa207f584fdd6
yayahc <yayahc@localhost.localdomain>
2023-09-29 22:42:52 +0000

    ++

---
+++ lib/features/name/data/repositories/name_repository_impl.2.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:ninety/features/name/data/models/i_name_model.dart';
+import '../../../../core/error/app_error.dart';
+import '../../domain/repositories/name_repository/i_name_repository.dart';
+import 'i_name_data_sources_repository.dart';
+
+class NameRepositoryImpl implements INameRepository {
+  final INameDataSource _nameDataSource;
+
+  NameRepositoryImpl(this._nameDataSource);
+
+  @override
+  Future<Either<AppError, List<ITranslationModel>>> getNames(String lang) {
+    return _nameDataSource.getNames(lang);
+  }
+}
