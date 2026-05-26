3c1fbc6ea3b359168c93a4ef4403ee518f56f4a2
Yaya HC <hamedcuenca5@gmail.com>
2026-05-26 19:31:41 +0000

    clean: remove unused setting screen

--- lib/router.dart
+++ lib/router.dart
@@ -5,34 +5,28 @@ import 'package:ninety/presentation/screens/favorite_name_screen.dart';
 import 'package:ninety/presentation/screens/home_screen.dart';
 import 'package:ninety/presentation/screens/name_item_screen.dart';
 import 'package:ninety/presentation/screens/quiz_screen.dart';
-import 'package:ninety/presentation/screens/settings_screen.dart';
 
 class AppRouter {
   static final GoRouter router =
       GoRouter(initialLocation: "/home", debugLogDiagnostics: true, routes: [
-        GoRoute(
-          path: "/home",
-          builder: (context, state) => const HomeScreen(),
-        ),
-        GoRoute(
-          path: "/favorite",
-          builder: (context, state) => const FavoriteNameScreen(),
-        ),
-        GoRoute(
-          path: "/quiz",
-          builder: (context, state) => const QuizScreen(),
-        ),
-        GoRoute(
-          path: "/settings",
-          builder: (context, state) => const SettingsScreen(),
-        ),
-        GoRoute(
-          pageBuilder: (context, state) {
-            final name = state.extra as Name;
-            return MaterialPage<void>(child: NameItemScreen(name: name));
-          },
-          path: "/name",
-        ),
-      ]);
+    GoRoute(
+      path: "/home",
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: "/favorite",
+      builder: (context, state) => const FavoriteNameScreen(),
+    ),
+    GoRoute(
+      path: "/quiz",
+      builder: (context, state) => const QuizScreen(),
+    ),
+    GoRoute(
+      pageBuilder: (context, state) {
+        final name = state.extra as Name;
+        return MaterialPage<void>(child: NameItemScreen(name: name));
+      },
+      path: "/name",
+    ),
+  ]);
 }
-
